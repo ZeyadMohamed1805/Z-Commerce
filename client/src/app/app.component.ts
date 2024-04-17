bdb6934a635f322ba48502fa5694d1471e45fb71
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-04-17 18:11:15 +0200

    Added Categories To The Layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { CategoriesComponent } from './components/layout/categories/categories.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CategoriesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { MatSidenavContainer } from '@angular/material/sidenav';
-import { CategoriesComponent } from '../categories/categories.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CategoriesComponent, MatSidenavContainer],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
