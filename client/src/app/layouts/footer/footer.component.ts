4949e381476e9aa471844a1df743109f7b96924c
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-04-18 16:59:19 +0200

    Completed The Footer Layout Component

--- client/src/app/layouts/footer/footer.component.ts
+++ client/src/app/layouts/footer/footer.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDivider } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatAutocompleteModule,
+    MatInputModule,
+    MatDivider,
+    RouterLink,
+    CommonModule,
+  ],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  options: Array<string> = ['One', 'Two', 'Three'];
+  navLinks: Array<string> = ['home', 'products', 'sellers'];
+  navButtons: Array<string> = ['login', 'register'];
 }
