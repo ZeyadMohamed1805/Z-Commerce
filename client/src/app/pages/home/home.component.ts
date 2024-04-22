fcbddbffe342fb308288216c639b2bfb615c7ce8
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-04-22 05:29:18 +0200

    Initialized The Home Page

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,11 +1,86 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { ProductCardComponent } from '../../components/cards/product-card/product-card.component';
+import { SellerCardComponent } from '../../components/cards/seller-card/seller-card.component';
+import { TProduct } from '../../components/cards/product-card/product-card.types';
+import { TSeller } from '../../components/cards/seller-card/seller-card.types';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [
+    CarouselComponent,
+    ProductCardComponent,
+    SellerCardComponent,
+    MatDividerModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  productCards: Array<TProduct> = [
+    {
+      _id: 'ID0',
+      _v: 'V',
+      name: 'Name',
+      photo: '../../../../assets/images/light-logo.svg',
+      seller: 'Seller',
+      categories: ['One', 'Two', 'Three'],
+      rating: 5,
+      price: 199,
+      creationDate: new Date(),
+      description: 'Description',
+    },
+    {
+      _id: 'ID1',
+      _v: 'V',
+      name: 'Name',
+      photo: '../../../../assets/images/light-logo.svg',
+      seller: 'Seller',
+      categories: ['One', 'Two', 'Three'],
+      rating: 5,
+      price: 199,
+      creationDate: new Date(),
+      description: 'Description',
+    },
+    {
+      _id: 'ID2',
+      _v: 'V',
+      name: 'Name',
+      photo: '../../../../assets/images/light-logo.svg',
+      seller: 'Seller',
+      categories: ['One', 'Two', 'Three'],
+      rating: 5,
+      price: 199,
+      creationDate: new Date(),
+      description: 'Description',
+    },
+  ];
+  sellerCards: Array<TSeller> = [
+    {
+      _id: 'ID0',
+      _v: 'V',
+      name: 'Name',
+      photo: '../../../../assets/images/light-logo.svg',
+      rating: 5,
+      products: ['One', 'Two', 'Three'],
+    },
+    {
+      _id: 'ID1',
+      _v: 'V',
+      name: 'Name',
+      photo: '../../../../assets/images/light-logo.svg',
+      rating: 5,
+      products: ['One', 'Two', 'Three'],
+    },
+    {
+      _id: 'ID2',
+      _v: 'V',
+      name: 'Name',
+      photo: '../../../../assets/images/light-logo.svg',
+      rating: 5,
+      products: ['One', 'Two', 'Three'],
+    },
+  ];
+}
