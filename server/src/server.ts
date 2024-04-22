b6dec8c537e61ebb55157c4d5d5f9b3d85b2813d
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-04-22 19:29:57 +0200

    Connected Database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
+// Imports
 import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+// Access Environment Variables
 dotenv.config();
 
+// Initialize Server & Port Number
 const port = process.env.PORT || 5000;
 const server: Express = express();
 
@@ -10,4 +14,17 @@ server.get("/", (request: Request, response: Response) =>
 	response.status(200).end("Welcome To Z-Commerce Server")
 );
 
-server.listen(port, () => console.log(`Server is running on port ${port}`));
+// Connect To Database
+const connectDatabase = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO!);
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Run The Server
+server.listen(port, () => {
+	connectDatabase();
+	console.log("Connected to Backend...");
+});
