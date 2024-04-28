aba11af2cdcaaf64bd7206ba40a12404f0eed178
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-04-28 23:45:58 +0300

    Completed The Basket Alerts

--- client/src/app/pages/basket/basket.component.ts
+++ client/src/app/pages/basket/basket.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CartComponent } from '../../components/cart/cart.component';
 import { WishlistComponent } from '../../components/wishlist/wishlist.component';
 import { MatSelectModule } from '@angular/material/select';
@@ -23,39 +23,14 @@ import { MatBadgeModule } from '@angular/material/badge';
   styleUrl: './basket.component.scss',
 })
 export class BasketComponent {
-  displayedColumns: string[] = ['product', 'price', 'quantity', 'subtotal'];
-  displayedColumnsTwo: string[] = ['title', 'value'];
-  amounts: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  ELEMENT_DATA: Array<{
-    product: { image: string; name: string };
-    price: number;
-    quantity: number;
-    subtotal: number;
-  }> = [
-    {
-      product: { image: '../../../assets/images/light-logo.svg', name: 'Name' },
-      price: 250,
-      quantity: 0,
-      subtotal: 200,
-    },
-    {
-      product: { image: '../../../assets/images/light-logo.svg', name: 'Name' },
-      price: 250,
-      quantity: 0,
-      subtotal: 200,
-    },
-    {
-      product: { image: '../../../assets/images/light-logo.svg', name: 'Name' },
-      price: 250,
-      quantity: 0,
-      subtotal: 200,
-    },
-  ];
-  ELEMENT_DATA_TWO = [
-    { title: 'Subtotal', value: 1750 },
-    { title: 'Shipping', value: 0 },
-    { title: 'Total', value: 1750 },
-  ];
-  dataSource = this.ELEMENT_DATA;
-  dataSourceTwo = this.ELEMENT_DATA_TWO;
+  cartQuantity = JSON.parse(localStorage.getItem('cart')!).length
+    ? JSON.parse(localStorage.getItem('cart')!)
+        .map((item: any) => item.amount)
+        .reduce((current: any, next: any) => current + next)
+    : 0;
+  wishlistQuantity = JSON.parse(localStorage.getItem('wishlist')!).length
+    ? JSON.parse(localStorage.getItem('wishlist')!)
+        .map((item: any) => item.amount)
+        .reduce((current: any, next: any) => current + next)
+    : 0;
 }
