4bb41760b3987dc019b58ad4aafb899c4d957c11
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-02 21:18:11 +0300

    Restricted Every Product To One Category

--- client/src/app/components/cards/product-card/product-card.component.ts
+++ client/src/app/components/cards/product-card/product-card.component.ts
@@ -6,11 +6,18 @@ import { TProduct } from './product-card.types';
 import { MatIconModule } from '@angular/material/icon';
 import { ApiService } from '../../../services/api/api.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { SubstringPipe } from '../../../pipes/substring/substring.pipe';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatChipsModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    SubstringPipe,
+  ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
