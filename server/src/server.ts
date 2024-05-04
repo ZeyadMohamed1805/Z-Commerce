4ce43890c981747388e8e089cedbb8df3c1a53c5
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-04 07:10:36 +0300

    Updated Base URL

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ const server: Express = express();
 // Middlewares
 server.use(
 	cors({
-		origin: ["https://z-commerce-rho.vercel.app/"],
+		origin: ["https://z-commerce-rho.vercel.app"],
 	})
 );
 server.use(function (request, res: Response, next: NextFunction) {
