2f5d5f851335296990cd5862de029fd38e5d37c0
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-05-05 03:29:15 +0300

    Removed Dist Folder From Server

--- server/dist/types/products.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EProductState = void 0;
-var EProductState;
-(function (EProductState) {
-    EProductState[EProductState["Out Of Stock"] = 0] = "Out Of Stock";
-    EProductState[EProductState["In Stock"] = 1] = "In Stock";
-})(EProductState || (exports.EProductState = EProductState = {}));
--- server/dist/types/users.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EUserRole = void 0;
-var EUserRole;
-(function (EUserRole) {
-    EUserRole[EUserRole["buyer"] = 0] = "buyer";
-    EUserRole[EUserRole["seller"] = 1] = "seller";
-})(EUserRole || (exports.EUserRole = EUserRole = {}));
